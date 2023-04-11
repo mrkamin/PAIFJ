17896f83c4ba52d44595a390da95fd2ef14ba082
Mohammad Rafi Amin <mrkamin2@gmail.com>
2023-04-12 04:17:19 +0500

    Styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ import style from '../style/Navbar.module.css';
 const Navbar = () => {
   return (
     <div className={style.navBarContainer}>
-      
+
       <img className={style.navBarLogo} src={paifjlogo} alt="PAIFJLogo" />
       
       </div>
