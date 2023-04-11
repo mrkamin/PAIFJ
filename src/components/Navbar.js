340415f6f71d830ba39b65f961dd27c4cac962d7
Mohammad Rafi Amin <mrkamin2@gmail.com>
2023-04-12 03:31:08 +0500

    Rool Back

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,75 +6,7 @@ const Navbar = () => {
   return (
     <div className={style.navBarContainer}>
       
-      
-      <nav id="navbar" class="navbar navbar-expand-lg fixed-top">
-      <div class="container">
-        <a class="navbar-brand" href="#"><img className={style.navBarLogo} src={paifjlogo} alt="PAIFJLogo" /></a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav mx-auto">
-            <li class="nav-item">
-              <a class="nav-link" href="#home">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#services">Services</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#work">Work</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#contact">Contact</a>
-            </li>
-          </ul>
-          <ul class="navbar-nav flex-row">
-            
-            <li class="nav-item">
-              <a
-                class="social-icon"
-                href="https://www.instagram.com/rafikambiz/"
-                ><i class="ri-instagram-line"></i
-              ></a>
-            </li>
-            <li class="nav-item">
-              <a
-                class="social-icon"
-                href="https://www.facebook.com/profile.php?id=100064317682546"
-                ><i class="ri-facebook-line"></i
-              ></a>
-            </li>
-            <li class="nav-item">
-              <a
-                class="social-icon"
-                href="https://www.youtube.com/channel/UCnNGRwjg9TrN8kEWggQhS-g"
-                ><i class="ri-youtube-line"></i
-              ></a>
-            </li>
-            <li class="nav-item">
-              <a
-                class="social-icon"
-                href="https://www.linkedin.com/in/mohammad-rafi-amin-63b4319b/"
-                ><i class="ri-linkedin-line"></i
-              ></a>
-            </li>
-            <li class="nav-item">
-              <a class="social-icon" href="https://twitter.com/Mohamma63974237"
-                ><i class="ri-twitter-line"></i
-              ></a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+      <img className={style.navBarLogo} src={paifjlogo} alt="PAIFJLogo" />
       
       </div>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-/* import style from '../style/Home.module.css'; */
+import style from '../style/Home.module.css';
 import Slider from '../components/Slider/Slider';
 import ReadMore from '../components/Slider/ReadMore';
 
@@ -11,137 +11,65 @@ const Home = () => {
         <ReadMore />
       </div>
       
-      <section id="contact" class="section-connector">
-      <div class="container section-padding">
-        <div class="row">
-          <div class="col-12">
-            <div
-              class="section-title text-center"
-              data-aos="fade-up"
-              data-aos-delay="100"
-            >
-              <h1>Contact</h1>
-              <p>
-               PAIFJ is a ......... Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus quia animi commodi fugiat optio neque, eveniet aspernatur consectetur amet. Labore eveniet numquam tenetur tempora ducimus accusamus. Voluptatibus dolorum cum cupiditate?
-              </p>
-              <div class="line"></div>
-            </div>
-          </div>
-        </div>
-        <div class="row justify-content-center">
-          <div class="col-md-8" data-aos="fade-up" data-aos-delay="200">
-            <form action="https://formspree.io/f/maykokoe" method="post" class="row g-3">
-              <div class="form-group col-md-4">
-                <input type="text" class="form-control" placeholder="Name" required name="name" maxlength="30"/>
-              </div>
-              <div class="form-group col-md-4">
+      <div className={style.homeContact} id="contact">
+      <footer className={style.homeFooter} id="footer">
+        <div className={style.homeContainer}>
+          <div className={style.homeContaineritems}>
+            <h3>Contact Us</h3>
+            <p>
+             if you have...........
+            </p>
+            <form action="https://formspree.io/f/maykokoe" method="post">
+              <div class="form__control">
                 <input
                   type="text"
-                  class="form-control"
-                  placeholder="Phone Number" 
+                  placeholder="Name"
+                  class="name"
+                  id="name"
+                  maxlength="30"
+                  required
+                  name="name"
                 />
               </div>
-              <div class="form-group col-md-4">
+              <div class="form__control">
                 <input
                   type="email"
-                  class="form-control"
-                  placeholder="Email Address" required name="email"
+                  placeholder="name@example.com"
+                  class="email"
+                  id="email"
+                  name="email"
+                  required
                 />
               </div>
-              <div class="form-group col-md-12">
-                <input
-                  type="text"
-                  class="form-control"
-                  placeholder="Enter Subject" required name="text"
-                />
-              </div>
-              <div class="form-group col-md-12">
+              <div class="form__control">
                 <textarea
-                  name=""
-                  class="form-control"
-                  placeholder="Enter Your Message"
-                  rows="4"
+                  cols="30"
+                  rows="10"
+                  class="textarea"
+                  id="textarea"
+                  placeholder="Write your message here"
+                  maxlength="500"
+                  name="textarea"
+                  required
                 ></textarea>
               </div>
-              <div class="form-group text-center col-md-12">
-                <button class="btn btn-brand" type="submit">Send Message</button>
+              <div class="form__control">
+                <input
+                  type="submit"
+                  placeholder=""
+                  class="submit"
+                  id="submit"
+                  value="Get in touch"
+                />
               </div>
             </form>
           </div>
         </div>
-      </div>
-      <footer>
-        <div class="footer-top">
-          <div class="container">
-            <div class="row gy-5">
-              <div class="col-md-4">
-                <h4><a href="#" class="text-white">PAFIJ</a></h4>
-                <p>
-                  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Et
-                  eum nihil assumenda est quasi impedit quas praesentium
-                  voluptatibus, optio libero, repellat, officia magnam expedita
-                  itaque nam aut doloremque dolor excepturi!
-                </p>
-                <ul class="navbar-nav flex-row">
-                  
-                  <li class="nav-item">
-                    <a
-                      class="social-icon"
-                      href="https://www.youtube.com/channel/UCnNGRwjg9TrN8kEWggQhS-g"
-                      ><i class="ri-youtube-line"></i
-                    ></a>
-                  </li>
-                  <li class="nav-item">
-                    <a
-                      class="social-icon"
-                      href="https://www.linkedin.com/in/mohammad-rafi-amin-63b4319b/"
-                      ><i class="ri-linkedin-line"></i
-                    ></a>
-                  </li>
-                  <li class="nav-item">
-                    <a
-                      class="social-icon"
-                      href="https://twitter.com/Mohamma63974237"
-                      ><i class="ri-twitter-line"></i
-                    ></a>
-                  </li>
-                </ul>
-              </div>
-              <div class="col-md-3 ms-auto">
-                <h4>Quick Links</h4>
-                <ul>
-                  <li><a href="#services">Services</a></li>
-                  <li><a href="#blog">Blog</a></li>
-                  <li><a href="#contact">Contact</a></li>
-                </ul>
-              </div>
-              <div class="col-md-3">
-                <h4>Contact</h4>
-                <ul>
-                  <li>Email: mr.....2@gmail.com</li>
-                  <li>Whatsup: +93......</li>
-                  <li>Address: Islamabad Pakistan</li>
-                </ul>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="footer-bottom">
-          <div class="container">
-            <div class="row justify-content-between">
-              <div class="col-auto">
-                <p class="mb-0">Copyright text goes here</p>
-              </div>
-              <div class="col-auto">
-                <p class="mb-0">
-                  Template designed by <a href="#">MRK Amin</a>
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
       </footer>
-    </section>
+      <div>
+        <span className={style.homeFooter}>PAIFJ</span>
+      </div>
+    </div>
     </div>
   );
 };
